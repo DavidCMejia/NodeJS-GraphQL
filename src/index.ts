a82671e6adfba7dd4661f794e31bf817787dbfe9
David <dcmejiaz@geosat.com.co>
2022-06-06 11:04:45 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+// 1 - Query
+
+const typeDefs = gql`
+
+    type Query {
+        info: String!
+    }
+`;
+
+// 2 - Resolvers
+
+const resolvers = {
+    Query: {
+        info: () => 'Hello world'
+    }
+};
+
+// 3- Server
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen()
+    .then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
